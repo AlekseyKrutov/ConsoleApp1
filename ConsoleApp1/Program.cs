e8278fbb99d211c8eb043c8bfcb65843f99decf1
Aleksey Krutov <alekseykrutovvv@gmail.com>
2017-03-22 22:36:42 +0300

    я изменял файл

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("The value 99999 in other formats: ");
+            Console.WriteLine("c format: {0:C}", 99999);
+            Console.WriteLine("d9 format: {0:d9}", 99999);
+            Console.WriteLine("f3 format: {0:f3}", 99999);
+            Console.WriteLine("n format: {0:n}", 99999);
+
+            Console.WriteLine("E format: {0:E}", 99999);
+            Console.WriteLine("e format: {0:e}", 99999);
+
+            Console.WriteLine("X format: {0:X}", 99999);
+            Console.WriteLine("x format: {0:x}", 99999);
+
+            Console.WriteLine("asds");
+            Console.ReadLine();
         }
     }
 }
