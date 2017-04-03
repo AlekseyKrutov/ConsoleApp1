1128c222de25ea201d45c689f70f5f7d1faa596a
Aleksey Krutov <alekseykrutovvv@gmail.com>
2017-04-03 22:21:11 +0300

    я изменил

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Employee
+    {
+        private string empName;
+        private int empID;
+        private float currPay;
+        
+        public Employee () {}
+        public Employee (string name, int id, float pay)
+        {
+            empName = name;
+            empID = id;
+            currPay = pay;
+        }
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+        }
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", empName);
+            Console.WriteLine("ID: {0}", empID);
+            Console.WriteLine("Pay: {0}", currPay);
+        }
+        public string GetName()
+        {
+            return empName;
+        }
+        public void Setname(string name)
+        {
+            if (name.Length < 16)
+                Console.WriteLine("Error name < 16!");
+            else
+                empName = name;
+        }
+    }
+}
